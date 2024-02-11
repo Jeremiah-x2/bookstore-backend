73deb134f3d53249a08e400b0d5aa1b045e014d1
Jeremiah <jeremiahhussaini2@gmail.com>
2024-02-11 01:52:50 +0100

    made changes

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(cookieParser());
 app.use((req, res, next) => {
     res.header(
         'Access-Control-Allow-Origin',
-        'https://bookstore-kjo51mq51-jeremiah-x2s-projects.vercel.app'
+        'https://bookstore-beryl.vercel.app'
     );
     res.header(
         'Access-Control-Allow-Headers',
