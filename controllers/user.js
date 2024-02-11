5d4c6e020e191fd77905bd1a8c99c861fee232ee
Jeremiah <jeremiahhussaini2@gmail.com>
2024-02-11 02:54:41 +0100

    made changes

--- controllers/user.js
+++ controllers/user.js
@@ -27,7 +27,7 @@ exports.create_user = async (req, res) => {
         const token = generateToken(createUser._id);
         res.cookie('token', token, {
             maxAge: 1000 * 60 * 60 * 24,
-            sameSite: 'lax',
+            sameSite: 'none',
             secure: true,
         });
         res.status(201).json({
@@ -56,7 +56,7 @@ exports.login = (req, res) => {
                 const token = generateToken(user._id);
                 res.cookie('token', token, {
                     maxAge: 1000 * 60 * 60 * 24,
-                    sameSite: 'lax',
+                    sameSite: 'none',
                     secure: true,
                     // secure: false,
                 });
