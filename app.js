59de55371280271b80538a47d8217a6e68c4ff8d
Jeremiah <jeremiahhussaini2@gmail.com>
2024-02-12 14:56:05 +0100

    Changed some stuffs

--- app.js
+++ app.js
@@ -18,7 +18,10 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header(
+        'Access-Control-Allow-Origin',
+        'https://bookstore-beryl.vercel.app'
+    );
     res.header(
         'Access-Control-Allow-Headers',
         'Origin, X-Requested-With, Content-Type, Accept, Authorization, Cookie'
