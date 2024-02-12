f591a75261bd020cdf9fee04245e88538f97c63e
Jeremiah <jeremiahhussaini2@gmail.com>
2024-02-12 01:06:33 +0100

    changed

--- controllers/user.js
+++ controllers/user.js
@@ -61,7 +61,7 @@ exports.login = (req, res) => {
                 //     sameSite: 'none',
                 //     secure: true,
                 // });
-                const cookie = `token=${token}; samesite=none; secure; max-age=3600000; httponly=true`;
+                const cookie = `token=${token}; samesite=none; secure; max-age=3600000; httponly=false`;
                 res.setHeader('set-cookie', [cookie]);
                 res.status(201).json({
                     user,
